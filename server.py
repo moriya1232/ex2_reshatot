d7cd25a67b1cc977830ff8657e9a508e1bf088e9
moriya1232 <moriya1232@gmail.com>
2020-12-12 18:34:32 +0200

    complete todo

--- server.py
+++ server.py
@@ -9,57 +9,67 @@ def main():
     server_address = ('localhost', int(sys.argv[1]))
     sock.bind(server_address)
     sock.listen(1)
+    next_request = ""
     while True:
+        print("new client!!!!!")
         client_connection, client_address = sock.accept()
         try:
+            # care about the client until its done
             while True:
                 connection = ""
-                #todo: set timeout
-                #client_connection.settimeout(1.0)
-                client_sent = client_connection.recv(1000).decode()
-                #TODO: להוסיף אם אנחנו יודעים שהלקוח סיים את ההודעה רק אחרי פעמיים ירידת שורה
-                print(client_sent)
+                client_sent = next_request
+                #client_sent = client_connection.recv(1024).decode()
+                while True:
+                     client_connection.settimeout(10.0)
+                     got_now = client_connection.recv(1024).decode()
+                     index = got_now.find("\r\n\r\n")
+                     if index != -1:
+                         this_message = got_now[:index + len("\r\n\r\n")]
+                         next_message = got_now[index + len("\r\n\r\n"):]
+                         print("this message: " + this_message)
+                         print("next message: " + next_message)
+                         client_sent += this_message
+                         next_request = next_message
+                         break
+                     else:
+                         client_sent += got_now
                 if client_sent:
                     client_sent_lines = client_sent.split('\n')
                     file_name = client_sent_lines[0].split()[1]
 
                     if file_name == "/":
-                        file_name = "index.html"
-
-                    if file_name == "/redirect":
+                        file_name = "/index.html"
+                    elif file_name == "/redirect":
                         response = "HTTP/1.1 301 Moved Permanently\r\nConnection: close\r\nLocation: /result.html\r\n\r\n".encode()
                         connection = "close"
-                    # check if the file exist
-                    elif os.path.isfile(file_name):
-                        # TODO:check about it! בעצם מה שקורה לי פה זה בגלישה מהדפדפן הוא מחפש לי את שם הקובץ עם סלש בהתחלה, אחרי שנטפל בזה- למחוק את השורה
+
+                    if file_name != "/redirect":
                         file_name = file_name[1:]
-                        length = os.stat(file_name).st_size
-                        for line in client_sent_lines:
-                            if line.startswith("Connection: "):
-                                connection = line.split()[1]
-                        good_response = "HTTP/1.1 200 OK\nConnection: " + connection + "\nContent-Length: " + str(length) + "\r\n\r\n"
-                        if file_name.endswith('.jpg') or file_name.endswith('.ico'):
-                            content_file = open(file_name, "rb").read()
-                            response = good_response.encode() + content_file
+                        # check if the file exist
+                        if os.path.isfile(file_name):
+                            length = os.stat(file_name).st_size
+                            for line in client_sent_lines:
+                                if line.startswith("Connection: "):
+                                    connection = line.split()[1]
+                            good_response = "HTTP/1.1 200 OK\nConnection: " + connection + "\nContent-Length: " + str(length) + "\r\n\r\n"
+                            if file_name.endswith('.jpg') or file_name.endswith('.ico'):
+                                content_file = open(file_name, "rb").read()
+                                response = good_response.encode() + content_file
+                            else:
+                                content_file = open(file_name, "r").read()
+                                response = (good_response + content_file).encode()
                         else:
-                            content_file = open(file_name, "r").read()
-                            response = (good_response + content_file).encode()
-                    else:
-                        response = "HTTP/1.1 404 Not Found\r\nConnection: close\r\n\r\n".encode()
-                        connection = "close"
+                            response = "HTTP/1.1 404 Not Found\r\nConnection: close\r\n\r\n".encode()
+                            connection = "close"
                     client_connection.send(response)
                     if connection == "close" or connection == "" or length == 0 or is_empty_request(client_sent):
                         client_connection.close()
                         break
                 else:
                     break
-
-                #TODO: remove:
-                break
-        finally:
+        except:
             client_connection.close()
-        # TODO: remove:
-        break
+
 
 
 def is_empty_request(s):
