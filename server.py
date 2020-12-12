ee04388c09c7a997f20627f1ab2e8fd642b472c2
moriya <moriya1232@gmail.com>
2020-12-12 23:05:06 +0200

    care of small requests

--- server.py
+++ server.py
@@ -17,53 +17,35 @@ def main():
             while True:
                 connection = ""
                 client_sent = next_request
-                #client_sent = client_connection.recv(1024).decode()
                 while True:
-                     client_connection.settimeout(1.0)
-                     got_now = client_connection.recv(1024).decode()
-                     print(got_now, end="")
-                     index = got_now.find("\r\n\r\n")
-                     if index != -1:
-                         this_message = got_now[:index + len("\r\n\r\n")]
-                         next_message = got_now[index + len("\r\n\r\n"):]
-
-                         client_sent += this_message
-                         next_request = next_message
-                         break
-                     else:
-                         client_sent += got_now
-
+                    client_connection.settimeout(1.0)
+                    got_now = client_connection.recv(1024).decode()
+                    print(got_now, end="")
+                    index = got_now.find("\r\n\r\n")
+                    # the message dont have \r\n\r\n
+                    if index == -1:
+                        client_sent += got_now
+                    # found /r/n/r/n
+                    else:
+                        this_message = got_now[:index + len("\r\n\r\n")]
+                        next_message = got_now[index + len("\r\n\r\n"):]
+                        client_sent += this_message
+                        next_request = next_message
+                        index = next_message.find("\r\n\r\n")
+                        # found \r\n\r\n
+                        while index != -1:
+                            connection = care_request(client_connection, client_sent)
+                            if connection == "close":
+                                break
+                            this_message = next_request[:index + len("\r\n\r\n")]
+                            next_message = next_request[index + len("\r\n\r\n"):]
+                            client_sent = this_message
+                            next_request = next_message
+                            index = next_message.find("\r\n\r\n")
+                        break
                 if client_sent:
-                    client_sent_lines = client_sent.split('\n')
-                    file_name = client_sent_lines[0].split()[1]
-
-                    if file_name == "/":
-                        file_name = "/index.html"
-                    elif file_name == "/redirect":
-                        response = "HTTP/1.1 301 Moved Permanently\r\nConnection: close\r\nLocation: /result.html\r\n\r\n".encode()
-                        connection = "close"
-
-                    if file_name != "/redirect":
-                        file_name = file_name[1:]
-                        # check if the file exist
-                        if os.path.isfile(file_name):
-                            length = os.stat(file_name).st_size
-                            for line in client_sent_lines:
-                                if line.startswith("Connection: "):
-                                    connection = line.split()[1]
-                            good_response = "HTTP/1.1 200 OK\nConnection: " + connection + "\nContent-Length: " + str(length) + "\r\n\r\n"
-                            if file_name.endswith('.jpg') or file_name.endswith('.ico'):
-                                content_file = open(file_name, "rb").read()
-                                response = good_response.encode() + content_file
-                            else:
-                                content_file = open(file_name, "r").read()
-                                response = (good_response + content_file).encode()
-                        else:
-                            response = "HTTP/1.1 404 Not Found\r\nConnection: close\r\n\r\n".encode()
-                            connection = "close"
-                    client_connection.send(response)
-                    if connection == "close" or connection == "" or length == 0 or is_empty_request(client_sent):
-                        client_connection.close()
+                    connection = care_request(client_connection, client_sent)
+                    if connection == "close":
                         break
                 else:
                     break
@@ -71,11 +53,47 @@ def main():
             client_connection.close()
 
 
+def care_request(client_connection, client_sent):
+    client_sent_lines = client_sent.split('\n')
+    file_name = client_sent_lines[0].split()[1]
+    connection = "close"
+    if file_name == "/":
+        file_name = "/index.html"
+    elif file_name == "/redirect":
+        response = "HTTP/1.1 301 Moved Permanently\r\nConnection: close\r\nLocation: /result.html\r\n\r\n".encode()
+        connection = "close"
+
+    if file_name != "/redirect":
+        file_name = file_name[1:]
+        # check if the file exist
+        if os.path.isfile(file_name):
+            length = os.stat(file_name).st_size
+            for line in client_sent_lines:
+                if line.startswith("Connection: "):
+                    connection = line.split()[1]
+            good_response = "HTTP/1.1 200 OK\nConnection: " + connection + "\nContent-Length: " + str(
+                length) + "\r\n\r\n"
+            if file_name.endswith('.jpg') or file_name.endswith('.ico'):
+                content_file = open(file_name, "rb").read()
+                response = good_response.encode() + content_file
+            else:
+                content_file = open(file_name, "r").read()
+                response = (good_response + content_file).encode()
+        else:
+            response = "HTTP/1.1 404 Not Found\r\nConnection: close\r\n\r\n".encode()
+            connection = "close"
+    client_connection.send(response)
+    if connection == "close" or connection == "" or is_empty_request(client_sent):
+        client_connection.close()
+        connection = "close"
+    return connection
+
 
 def is_empty_request(s):
     if len(s.replace('\r\n', '').replace('\n', '').replace(' ', '')) == 0:
         return True
     return False
+
 
 if __name__ == "__main__":
     main()
